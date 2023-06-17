45807fcccd24d272feb506e361a22c676d847f63
Tyler Nguyen <tylerkn1@uci.edu>
2023-06-17 16:02:56 -0700

    initial commit

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        # animal = request.form["animal"]
+        todo = request.form["name","duration","priority"]
+        print(todo)
+        #busy = get from google calendar
+
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(todo),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,14 +26,44 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+'''
+todo qualities: [(name, duration, priority), (name, duration, priority), (name, duration, priority)]
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+-name
+-duration
+-priority
+maybe
+-fatigue
+-location
+'''
+
+def generate_prompt(todo):
+    return """repeat what i typed""".format(
+        todo.capitalize()
     )
+
+
+
+
+# def generate_prompt(todo):
+#     return """Create a schedule for me that puts 'todo tasks' at times when I am not also busy with 'busy tasks' ".
+
+# todo: 
+# schedule: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+# todo: Dog
+# schedule: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+# todo: {}
+# schedule:""".format(
+#         todo.capitalize()
+#     )
+
+# def generate_prompt(animal):
+#     return """Suggest three names for an animal that is a superhero.
+
+# Animal: Cat
+# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+# Animal: Dog
+# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+# Animal: {}
+# Names:""".format(animal.capitalize())
+
---
+++ locations.py
@@ -0,0 +1,10 @@
+import geocoder
+
+locationDict
+
+    #test code, we need to return data back from flask after this point
+locationDict = {['2333 piedmont ave, Berkeley, CA'], ['Birge Path, Berkeley, CA 94704']}
+
+g = geocoder.bing(locationDict, method='batch')
+for results in g:
+    print(results.city)
